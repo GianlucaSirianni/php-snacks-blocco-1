fde9f2ae545a5b27d187139ac21a07574c09246b
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-12-13 16:06:51 +0100

    es 2-3-4

---
+++ snack4/index.php
@@ -0,0 +1,20 @@
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+$array = [];
+
+
+while(count($array) < 15) {
+    $num = rand(1, 100);
+    if(!in_array($num, $array)){
+      $array[] = $num;
+    }
+  } 
+
+  echo "<pre>";
+  var_dump($array);
+  echo "</pre>";
+
+
+?>
